423ae56ff2901c9151b011790b2985bae789ce46
Balazs Rigo <rigo.balazs86@gmail.com>
2022-08-16 22:50:34 +0200

    Add project files.

---
+++ ParalellProgramming/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ParalellProgramming
+{
+    internal class Program
+    {
+        public static void Write(char c)
+        {
+            int i = 1000;
+            while (i -- >0)
+            {
+                Console.Write(c);
+            }
+        }
+
+        public static void Write(object o)
+        {
+            int i = 1000;
+            while (i-- > 0)
+            {
+                Console.Write(o);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Task t = new Task(Write,"Hello");
+            t.Start();
+            Task.Factory.StartNew(Write,123);
+                                    
+            Console.WriteLine("Main program done");
+            Console.ReadKey();
+        }
+    }
+}
